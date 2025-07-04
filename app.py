df147d425f7210e455e1624375d327bc64d76369
Kalpiekanayake <ekanayake.emkn@gmail.com>
2025-07-05 00:48:35 +0530

    Type conversion

--- app.py
+++ app.py
@@ -11,5 +11,24 @@ print (first_name)
 print (is_online)
 
 
-name = input ("What is your name  ")
-print("Hello"+name)
+name = input ("What is your name: ")
+print("Hello "+name)
+
+birth_year = input("Enter your birth year: ")
+age = 2025-int(birth_year)
+print(age)
+
+first = input("First number: ")
+second = input("Second number: ")
+sum = int(first)+int(second)
+print(sum)
+
+first = input("First number: ")
+second = input("Second number: ")
+sum = float(first)+float(second)
+print("Sum is: "+str(sum))
+
+first = float(input("First"))
+second = float (input("Second"))
+sum =  first+second
+print(sum)
